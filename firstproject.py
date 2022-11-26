329edf76a53ace6ae87203d47df2655269e46bac
sharumohammad <sharumd208@gmail.com>
2022-11-26 10:11:17 +0530

    first commit

---
+++ firstproject.py
@@ -0,0 +1,17 @@
+a=int(input())
+b=str(a)
+e=len(b)
+c=b[::-1]
+d=True
+for i in range(e):
+    if(b[i] != c[i]):
+        d=False
+        break
+isprime=True
+for i in range(2,a):
+    if(a%i==0):
+        isprime=False
+if(d==True and isprime==True):
+    print("It is a Prime palindrome")
+else:
+    print("It is not a Prime palindrome")
